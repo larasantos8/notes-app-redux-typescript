eb7ec8ef0938cdf14f419208fff5a95d11f2d27f
Lara Santos <lara.santos@venturus.org.br>
2020-10-09 08:50:32 -0300

    Learning redux with typescript

---
+++ src/NewNoteInput.tsx
@@ -0,0 +1,27 @@
+import React, { ChangeEvent } from 'react';
+
+interface NewNoteInputProps {
+    addNote(note:string): void;
+}
+
+const NewNoteInput:React.FC<NewNoteInputProps> = ({addNote}) => {
+    const [note, setNote ] = React.useState('')
+
+    const updateNote = (event:ChangeEvent<HTMLInputElement>) => {
+        setNote(event.target.value)
+    }
+
+    const onAddNoteClick = () => {
+        addNote(note)
+        setNote('')
+    }
+
+    return (
+      <div>
+        <input onChange={updateNote} value={note} type="text" name="note" placeholder="Note"></input>
+        <button onClick={onAddNoteClick}>Add note</button>
+      </div>
+    );
+};
+
+export default NewNoteInput;
